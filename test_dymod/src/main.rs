936ab5bde84ca1f82b7ff2948f82171e35c135d6
Vi <1149443+mistodon@users.noreply.github.com>
2018-03-21 19:00:41 +0000

    Clean up messages in test binary

--- test_dymod/src/main.rs
+++ test_dymod/src/main.rs
@@ -14,23 +14,20 @@ dymod!
 
 fn main()
 {
-    println!();
-
     #[cfg(debug_assertions)]
-    {
-        println!("You are running in debug mode.");
-        println!("Make changes to subcrate/src/lib.rs");
-        println!("Then run `cargo build` in the subcrate directory.");
-        println!("You should see your changes apply while this code runs:");
-    }
+    const MESSAGE: &str = "
+You are running in debug mode.
+Make changes to subcrate/src/lib.rs
+Then run `cargo build` in the subcrate directory.
+You should see your changes apply while this code runs:";
 
     #[cfg(not(debug_assertions))]
-    {
-        println!("You are running in release mode.");
-        println!("The `subcrate` module has been statically linked.");
-        println!("Any changes made to subcrate/src/lib.rs will not apply until this program is recompiled.");
-    }
+    const MESSAGE: &str = "
+You are running in release mode.
+The `subcrate` module has been statically linked.
+Any changes made to subcrate/src/lib.rs will not apply until this program is recompiled.";
 
+    println!("{}", MESSAGE);
     println!("\nPress Ctrl+C to quit.\n");
 
     loop
